9a68815d219d3ccd8aa2d3ea84b18f4dfc6e4736
nicardojailallBYU <130989328+nicardojailallBYU@users.noreply.github.com>
2023-05-31 09:37:25 -0400

    Initial-Upload

--- week 7/scripts/main.js
+++ week 7/scripts/main.js
@@ -3,13 +3,21 @@ const imageObserver = new IntersectionObserver((entries, observer) => {
       if (entry.isIntersecting) {
         const img = entry.target;
         const src = img.getAttribute("data-src");
+        const placeholderDuration = 1000; 
   
         if (src) {
-          img.src = src;
-          img.removeAttribute("data-src");
-          img.addEventListener("load", () => {
-            img.style.opacity = 1;
-          });
+          setTimeout(() => {
+            img.src = src;
+            img.removeAttribute("data-src");
+            img.addEventListener("load", () => {
+              img.style.opacity = 1;
+              img.style.transition = "opacity 0.5s";
+            });
+            img.addEventListener("error", () => {
+              img.style.opacity = 1;
+              img.style.transition = "opacity 0.5s";
+            });
+          }, placeholderDuration);
         }
   
         observer.unobserve(img);
@@ -19,5 +27,7 @@ const imageObserver = new IntersectionObserver((entries, observer) => {
   
   const images = document.querySelectorAll("img[data-src]");
   images.forEach((img) => {
+    img.style.opacity = 0;
     imageObserver.observe(img);
   });
+  
