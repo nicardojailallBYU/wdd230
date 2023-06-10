82a02d4b7d8bf728c242d1b7380654dbf11469ca
nicardojailallBYU <130989328+nicardojailallBYU@users.noreply.github.com>
2023-06-10 09:25:14 -0400

    Initial-Update

--- chamber/join/scripts/hidden.js
+++ chamber/join/scripts/hidden.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    // Get the current date and time
+    
     var currentDate = new Date();
     
-    // Format the date and time as desired (adjust as needed)
+    
     var formattedDateTime = currentDate.toISOString();
     
-    // Set the value of the hidden field
+    
     document.getElementById('hidden-field').value = formattedDateTime;
   });
 
