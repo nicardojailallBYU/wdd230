db33df84feb56cdb157a72e9823e42bf1c7167e0
nicardojailallBYU <130989328+nicardojailallBYU@users.noreply.github.com>
2023-06-21 15:01:12 -0400

    Initial-Update

---
+++ week 10/scripts/scripts.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=697da629c5ec240bbf898bcce7f5855d";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function displayResults(weatherData) {
+    currentTemp.innerHTML = weatherData.main.temp.toFixed(0);
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
+
+apiFetch();
+
+
