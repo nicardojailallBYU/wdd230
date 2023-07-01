bf7d9d69cebd42e6242cd6a9677f4aeffbeb8282
nicardojailallBYU <130989328+nicardojailallBYU@users.noreply.github.com>
2023-07-01 08:34:46 -0400

    Delete chamber directory

--- chamber/contact/scripts/banner.js
+++
@@ -1,8 +0,0 @@
-var currentDay = new Date().getDay();
-var banner = document.getElementById('banner');
-
-if (currentDay === 1 || currentDay === 2) {
-  banner.style.display = 'block'; // Show the banner
-} else {
-  banner.style.display = 'none'; // Hide the banner on other days
-}
