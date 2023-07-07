ab0c2c5f080818fb7cb207e6f20a8518fd36ff5c
nicardojailallBYU <130989328+nicardojailallBYU@users.noreply.github.com>
2023-07-07 08:14:35 -0400

    Delete bountiful foods directory

--- bountiful foods/scripts/hamburger.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  document.getElementById("primary-nav").classList.toggle("open");
-  document.getElementById("hamburger-btn").classList.toggle("open");
-}
-const x = document.getElementById('hamburger-btn');
-x.onclick = toggleMenu;
